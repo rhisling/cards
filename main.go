748419fc457a3d4c548972db4c184c25b02f527b
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-05-08 21:59:21 -0700

    Adding tostring method

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 
 	hand, remainingDeck := deal(cards, 5)
 	hand.print()
 	remainingDeck.print()
+
+	fmt.Println(cards.toString())
 }
