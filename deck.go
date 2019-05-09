0233939707eb2f9e20bc2068271d0371e9e2f03c
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-05-08 22:17:03 -0700

    Adding save from file

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -25,12 +26,22 @@ func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
-func (d deck) toString() string{
-	return strings.Join([] string(d),",")
+func (d deck) toString() string {
+	return strings.Join([] string(d), ",")
 }
 
-func (d deck)saveToFile(filename string) error{
-	return ioutil.WriteFile(filename, []byte(d.toString()),0666)
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile("my_cards")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
 
 func (d deck) print() {
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 func main() {
 	cards := newDeck()
 
-	_ = cards.saveToFile("my_cards")
+	cards= newDeckFromFile("my_cards")
+	cards.print()
 }
